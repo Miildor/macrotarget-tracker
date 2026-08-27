2a196c58ca8f045f4ba806a50639ae67edc4f96d
Miildor <erik.182.e@gmail.com>
2026-08-27 11:21:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="recomp-v1";
+const CACHE="recomp-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js",
   "./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{
